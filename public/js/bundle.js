42e659c09ead5c127c9bf5bb7340dd3ff8c46586
pratik-das <pratik16082001@gmail.com>
2023-07-27 04:26:08 +0530

    commit

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1372,7 +1372,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61583" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53485" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
